e3a7df14262b78ab8b21b119c7c0b21a0926fd62
Mahadi001 <sadat.himel@gmail.com>
2021-09-09 13:15:36 +0600

    update

--- alpha/header.php
+++
@@ -1,8 +0,0 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="UTF-8">
-
-	<?php wp_head(); ?>
-    <link rel="stylesheet" type="text/css" href="<?php echo get_stylesheet_uri(); ?>">
-</head>
